bece746fd656592040d903cc4f8e5375f4c28ac5
alex0210 <a0210@yandex.ru>
2023-02-25 14:24:42 +0300

    Added DZ4

---
+++ seminar1/DZbest/DZ4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите целое число.");
+
+int inValue = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+while (number <= inValue)
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine(number);
+    }
+    number++;
+}
