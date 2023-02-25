a65a31d1779c80e1dbc1d7e99b9314e2ddf1f217
alex0210 <a0210@yandex.ru>
2023-02-25 14:14:37 +0300

    Added DZ3

---
+++ seminar1/DZbest/DZ3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.WriteLine("Введите целое число.");
+
+string inValue;
+
+inValue = Console.ReadLine();
+int value = Convert.ToInt32(inValue);
+if (value % 2 == 0)
+{
+    System.Console.WriteLine($"Число {value} , является четным.");
+}
+else
+{
+   System.Console.WriteLine($"Число {value} , не является четным."); 
+}
+
