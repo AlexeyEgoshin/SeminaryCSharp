b1d392dbc204a11f80217a826b26e351c9bfa660
alex0210 <a0210@yandex.ru>
2023-02-25 17:54:18 +0300

    Added Sem2DZ3

---
+++ seminar2/DZ3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 2: Напишите программу, 
+// которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+
+
+
+
+int number = ReadInt("Введите число ");
+
+if (number < 100)
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+else
+{
+System.Console.WriteLine(InCenter(number));    
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a)
+{
+while (a >= 1000)
+{
+    a = a / 10;
+}
+a = a % 10;
+return a;
+}
