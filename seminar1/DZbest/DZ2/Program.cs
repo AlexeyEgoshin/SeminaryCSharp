401b5aac6fb3b2581c7a9606a5f9d5d4379cfa4f
alex0210 <a0210@yandex.ru>
2023-02-25 14:04:50 +0300

    Added DZ2

---
+++ seminar1/DZbest/DZ2/Program.cs
@@ -0,0 +1,39 @@
+﻿// int Prompt(string message)
+// {
+//     System.Console.Write($"{message} >");
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// int a = 2;
+// int b = 3;
+// int c = 7;
+
+ int Prompt(string message)
+ {
+     System.Console.Write($"{message} >");
+     return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = Prompt("Введите первое число ");
+int b = Prompt("Введите второе число ");
+int c = Prompt("Введите третье число ");
+int max = 0;
+
+if (a > b & a > c)
+{
+    max = a;
+}
+else if (b > a & b > c)
+{
+    max = b;
+}
+else 
+{
+    max = c;
+}
+
+System.Console.WriteLine($"Максимальное число {max}");
